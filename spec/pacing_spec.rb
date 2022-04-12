17adbb68a0a87658c842972d4c99ad73ce940a96
Kevin Dias <diasks2@gmail.com>
2022-04-12 15:00:14 +0900

    Add error spec example

--- spec/pacing_spec.rb
+++ spec/pacing_spec.rb
@@ -17,4 +17,14 @@ RSpec.describe Pacing::Pacer do
       expect(Pacing::Pacer.new(iep: iep, date: date, non_business_days: non_business_days ).calculate).to eq({iep_service: [{start_date: '1-01-22', end_date: '1-01-23', type_of_service: 'Language Therapy', frequency: 6, interval: 'monthly', time_per_session_in_minutes: 30, completed_visits_for_current_interval: 7, extra_sessions_allowable: 1, interval_for_extra_sesssions_allowable: 'monthly', remaining_visits: 0, reset_date: '31-01-2022', pace: 4, pace_indicator: "🐇" }, {start_date: '1-01-22', end_date: '1-01-23', type_of_service: 'Physical Therapy', frequency: 6, interval: 'monthly', time_per_session_in_minutes: 30, completed_visits_for_current_interval: 7, extra_sessions_allowable: 1, interval_for_extra_sesssions_allowable: 'monthly', remaining_visits: 0, reset_date: '31-01-2022', pace: 4, pace_indicator: "🐇" }]})
     end
   end
+
+
+  describe 'error handling' do
+    it "should respond with friendly error message for a misformed date" do
+      iep = {iep_service: [{start_date: '1-01-22', end_date: '1-01-23', type_of_service: 'Language Therapy', frequency: 6, interval: 'monthly', time_per_session_in_minutes: 30, completed_visits_for_current_interval: 7, extra_sessions_allowable: '1 per month' }, {start_date: '1-01-22', end_date: '1-01-23', type_of_service: 'Physical Therapy', frequency: 6, interval: 'monthly', time_per_session_in_minutes: 30, completed_visits_for_current_interval: 7, extra_sessions_allowable: '1 per month' }]}
+      date = '22-1-2022'
+      non_business_days = ['25-1-2022']
+      expect { Pacing::Pacer.new(iep: iep, date: date, non_business_days: non_business_days ).calculate }.to raise_error('Dates should be formatted as a string in the format mm-dd-yyyy')
+    end
+  end
 end
