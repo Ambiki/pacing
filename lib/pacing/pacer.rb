592b9a3a9f26e44422de8f0f752e7eeaf13d138d
Lukong Isaac <lisaacjunior@yahoo.com>
2022-10-06 14:54:40 +0100

    ideas

--- lib/pacing/pacer.rb
+++ lib/pacing/pacer.rb
@@ -5,7 +5,7 @@ module Pacing
   class Pacer
     COMMON_YEAR_DAYS_IN_MONTH = [nil, 31, 28, 31, 30, 31, 30, 31, 31, 30, 31, 30, 31]
     attr_reader :school_plan, :date, :non_business_days, :state
-    
+
     def initialize(school_plan:, date:, non_business_days:, state: :us_tn)
       @school_plan = school_plan
       @non_business_days = non_business_days
@@ -23,16 +23,26 @@ module Pacing
       end
 
       @school_plan[:school_plan_services].each do |school_plan_service|
-        raise TypeError.new("School plan type must be a string and cannot be nil") if school_plan_service[:school_plan_type].class != String || school_plan_service[:school_plan_type].nil? 
+        raise TypeError.new("School plan type must be a string and cannot be nil") if school_plan_service[:school_plan_type].class != String || school_plan_service[:school_plan_type].nil?
+
         raise ArgumentError.new("School plan services start and end dates can not be nil") if school_plan_service[:start_date].nil? || school_plan_service[:end_date].nil?
+
         raise TypeError.new("School plan services start and end dates should be formatted as a string in the format mm-dd-yyyy") if school_plan_service[:start_date].class != String || !/(0[1-9]|1[012])-(0[1-9]|[12][0-9]|3[01])-(19|20)\d\d/.match?(school_plan_service[:start_date])
+
         raise TypeError.new("School plan services start and end dates should be formatted as a string in the format mm-dd-yyyy") if school_plan_service[:end_date].class != String || !/(0[1-9]|1[012])-(0[1-9]|[12][0-9]|3[01])-(19|20)\d\d/.match?(school_plan_service[:end_date])
-        raise TypeError.new("Type of service must be a string and cannot be nil") if school_plan_service[:type_of_service].class != String || school_plan_service[:type_of_service].nil? 
-        raise TypeError.new("Frequency must be an integer and cannot be nil") if school_plan_service[:frequency].class != Integer || school_plan_service[:frequency].nil? 
+
+        raise TypeError.new("Type of service must be a string and cannot be nil") if school_plan_service[:type_of_service].class != String || school_plan_service[:type_of_service].nil?
+
+        raise TypeError.new("Frequency must be an integer and cannot be nil") if school_plan_service[:frequency].class != Integer || school_plan_service[:frequency].nil?
+
         raise TypeError.new("Interval must be a string and cannot be nil") if school_plan_service[:interval].class != String || school_plan_service[:interval].nil?
+
         raise TypeError.new("Time per session in minutes must be an integer and cannot be nil") if school_plan_service[:time_per_session_in_minutes].class != Integer || school_plan_service[:time_per_session_in_minutes].nil?
+
         raise TypeError.new("Completed visits for current interval must be an integer and cannot be nil") if school_plan_service[:completed_visits_for_current_interval].class != Integer || school_plan_service[:completed_visits_for_current_interval].nil?
+
         raise TypeError.new("Extra sessions allowable must be an integer and cannot be nil") if school_plan_service[:extra_sessions_allowable].class != Integer || school_plan_service[:extra_sessions_allowable].nil?
+
         raise TypeError.new("Interval for extra sessions allowable must be a string and cannot be nil") if school_plan_service[:interval_for_extra_sessions_allowable].class != String || school_plan_service[:interval_for_extra_sessions_allowable].nil?
       end
     end
@@ -40,12 +50,31 @@ module Pacing
     def calculate
       # Pace = actual visits at point in time - expected visits to meet frequency at that point in time
       service = @school_plan[:school_plan_services][0]
-      puts expected_visits(start_date: parse_date(service[:start_date]), end_date: parse_date(service[:end_date]), frequency: service[:frequency], frequency: service[:interval]), "e dey work?"
+      puts expected_visits(start_date: parse_date(service[:start_date]), end_date: parse_date(service[:end_date]), frequency: service[:frequency], interval: service[:interval]), "e dey work?"
       return {school_plan_services: [{school_plan_type: 'IEP', start_date: '01-01-2022', end_date: '01-01-2023', type_of_service: 'Language Therapy', frequency: 6, interval: 'monthly', time_per_session_in_minutes: 30, completed_visits_for_current_interval: 7, extra_sessions_allowable: 1 , interval_for_extra_sessions_allowable: 'monthly', remaining_visits: 0, reset_date: '01-31-2022', pace: 4, pace_indicator: "🐇" }, {school_plan_type: 'IEP', start_date: '01-01-2022', end_date: '01-01-2023', type_of_service: 'Physical Therapy', frequency: 6, interval: 'monthly', time_per_session_in_minutes: 30, completed_visits_for_current_interval: 7, extra_sessions_allowable: 1 , interval_for_extra_sessions_allowable: 'monthly', remaining_visits: 0, reset_date: '01-31-2022', pace: 4, pace_indicator: "🐇" }]}
     end
 
     def expected_visits(start_date:, end_date:, frequency:, interval:)
-      visitable_days = business_days(start_date, end_date).count
+      start_of_treatment_date = parse_date("#{parse_date(@date).month}-#{@parse_date(start_date).day}-#{parse_date(@date).month}")
+
+      end_of_treatment_date = start_of_treatment_date + COMMON_YEAR_DAYS_IN_MONTH[(parse_date(@date)).month]
+
+      days_between = business_days(start_of_treatment_date, end_of_treatment_date)
+
+      days_passed(15) = business_days(start_of_treatment_date, @date)
+
+      days_between(30) == frequency(12)
+
+      (frequency/days_between) * days_passed
+
+
+      # visitable_days = business_days(start_date, start_date + COMMON_YEAR_DAYS_IN_MONTH[(parse_date(@date)).month]).count
+
+      # visitable_days_left = business_days(parse_date(@date), start_date + COMMON_YEAR_DAYS_IN_MONTH[(parse_date(@date)).month]).count
+
+      
+      
+      # puts parse_date(@date), "wellness", ((frequency.to_f / visitable_days) * (visitable_days - visitable_days_left)), "our expectation", frequency, "days left", visitable_days_left, visitable_days
     end
 
     def yearly_pacer
