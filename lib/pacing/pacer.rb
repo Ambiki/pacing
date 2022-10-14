fff40fd2c2839498ff23da6a612909e57a1dbfbf
nsoseka <lisaacjunior@yahoo.com>
2022-10-14 03:42:15 +0100

    Pacing Gem MVP Basic Version (#9)
    * mvp drafts
    
    * add holidays management
    
    * ideas
    
    * update pacer code
    
    * add support for weekly pacing
    
    * add yearly pacing
    
    * remove unused code and improve comments
    
    * refactor and include appropriate pace indicator emoji
    
    * better handling of lack of working days and refactor

--- lib/pacing/pacer.rb
+++ lib/pacing/pacer.rb
@@ -1,56 +1,215 @@
+require 'date'
+require 'holidays'
+
 module Pacing
   class Pacer
-    attr_reader :school_plan, :date, :non_business_days
-    def initialize(school_plan:, date:, non_business_days:)
+    COMMON_YEAR_DAYS_IN_MONTH = [nil, 31, 28, 31, 30, 31, 30, 31, 31, 30, 31, 30, 31]
+    attr_reader :school_plan, :date, :non_business_days, :state
+
+    def initialize(school_plan:, date:, non_business_days:, state: :us_tn)
       @school_plan = school_plan
+      @non_business_days = non_business_days
+      @date = date
+      @state = state
+
       raise ArgumentError.new("You must pass in at least one school plan") if @school_plan.nil?
       raise TypeError.new("School plan must be a hash") if @school_plan.class != Hash
-      @date = date
+      
       raise ArgumentError.new('You must pass in a date') if @date.nil?
       raise TypeError.new("The date should be formatted as a string in the format mm-dd-yyyy") if @date.class != String || !/(0[1-9]|1[012])-(0[1-9]|[12][0-9]|3[01])-(19|20)\d\d/.match?(@date)
-      @non_business_days = non_business_days
+      
       @non_business_days.each do |non_business_day|
         raise TypeError.new('"Non business days" dates should be formatted as a string in the format mm-dd-yyyy') if non_business_day.class != String || !/(0[1-9]|1[012])-(0[1-9]|[12][0-9]|3[01])-(19|20)\d\d/.match?(non_business_day)
       end
+
       @school_plan[:school_plan_services].each do |school_plan_service|
-        raise TypeError.new("School plan type must be a string and cannot be nil") if school_plan_service[:school_plan_type].class != String || school_plan_service[:school_plan_type].nil? 
+        raise TypeError.new("School plan type must be a string and cannot be nil") if school_plan_service[:school_plan_type].class != String || school_plan_service[:school_plan_type].nil?
+
         raise ArgumentError.new("School plan services start and end dates can not be nil") if school_plan_service[:start_date].nil? || school_plan_service[:end_date].nil?
+
         raise TypeError.new("School plan services start and end dates should be formatted as a string in the format mm-dd-yyyy") if school_plan_service[:start_date].class != String || !/(0[1-9]|1[012])-(0[1-9]|[12][0-9]|3[01])-(19|20)\d\d/.match?(school_plan_service[:start_date])
+
         raise TypeError.new("School plan services start and end dates should be formatted as a string in the format mm-dd-yyyy") if school_plan_service[:end_date].class != String || !/(0[1-9]|1[012])-(0[1-9]|[12][0-9]|3[01])-(19|20)\d\d/.match?(school_plan_service[:end_date])
-        raise TypeError.new("Type of service must be a string and cannot be nil") if school_plan_service[:type_of_service].class != String || school_plan_service[:type_of_service].nil? 
-        raise TypeError.new("Frequency must be an integer and cannot be nil") if school_plan_service[:frequency].class != Integer || school_plan_service[:frequency].nil? 
+
+        raise TypeError.new("Type of service must be a string and cannot be nil") if school_plan_service[:type_of_service].class != String || school_plan_service[:type_of_service].nil?
+
+        raise TypeError.new("Frequency must be an integer and cannot be nil") if school_plan_service[:frequency].class != Integer || school_plan_service[:frequency].nil?
+
         raise TypeError.new("Interval must be a string and cannot be nil") if school_plan_service[:interval].class != String || school_plan_service[:interval].nil?
+
         raise TypeError.new("Time per session in minutes must be an integer and cannot be nil") if school_plan_service[:time_per_session_in_minutes].class != Integer || school_plan_service[:time_per_session_in_minutes].nil?
+
         raise TypeError.new("Completed visits for current interval must be an integer and cannot be nil") if school_plan_service[:completed_visits_for_current_interval].class != Integer || school_plan_service[:completed_visits_for_current_interval].nil?
+
         raise TypeError.new("Extra sessions allowable must be an integer and cannot be nil") if school_plan_service[:extra_sessions_allowable].class != Integer || school_plan_service[:extra_sessions_allowable].nil?
+
         raise TypeError.new("Interval for extra sessions allowable must be a string and cannot be nil") if school_plan_service[:interval_for_extra_sessions_allowable].class != String || school_plan_service[:interval_for_extra_sessions_allowable].nil?
       end
     end
 
-    #  iep_interval = 12
-    #  date = '01-22-2022'
-    #  duration = 20
-    #  completed_visits = 14
-
-    # TODO Missing Data
-    # IEP reset date
-    # Frequency (weekly, monthly)
-
-
-    # IEP visits allowed per the period
-    # How many visits have happened in the period
-
-
-    # IEP (parent)
-
-      # IEP Services
-        # Service A (discipline, 2Wx30)
-        # Service B (3Mx30)
-        # Service C (4Mx20)
-
     def calculate
-      return {school_plan_services: [{school_plan_type: 'IEP', start_date: '01-01-2022', end_date: '01-01-2023', type_of_service: 'Language Therapy', frequency: 6, interval: 'monthly', time_per_session_in_minutes: 30, completed_visits_for_current_interval: 7, extra_sessions_allowable: 1 , interval_for_extra_sessions_allowable: 'monthly', remaining_visits: 0, reset_date: '01-31-2022', pace: 4, pace_indicator: "🐇" }, {school_plan_type: 'IEP', start_date: '01-01-2022', end_date: '01-01-2023', type_of_service: 'Physical Therapy', frequency: 6, interval: 'monthly', time_per_session_in_minutes: 30, completed_visits_for_current_interval: 7, extra_sessions_allowable: 1 , interval_for_extra_sessions_allowable: 'monthly', remaining_visits: 0, reset_date: '01-31-2022', pace: 4, pace_indicator: "🐇" }]}
+      services = @school_plan[:school_plan_services]
+
+      services = services.map do |service|
+        expected = expected_visits(start_date: parse_date(service[:start_date]), end_date: parse_date(service[:end_date]), frequency: service[:frequency], interval: service[:interval])
+
+        service[:pace] = pace(service[:completed_visits_for_current_interval], expected)
+        service[:reset_date] = reset_date(start_date: service[:start_date], interval: service[:interval_for_extra_sessions_allowable])
+        service[:remaining_visits] = remaining_visits(completed_visits: service[:completed_visits_for_current_interval], required_visits: service[:frequency])
+        service[:pace_indicator] = pace_indicator(service[:pace])
+
+        service
+      end
+
+      { school_plan_services: services }
     end
 
+    # get a spreadout of visit dates over an interval by using simple proportion.
+    def expected_visits(start_date:, end_date:, frequency:, interval:)
+      reset_start = start_of_treatment_date(start_date, interval)
+
+      reset_end =  reset_start + interval_days(interval)
+
+      days_between = business_days(reset_start, reset_end).count
+
+      days_passed = business_days(reset_start, parse_date(@date)).count
+
+      if days_between != 0
+        return ((frequency/days_between.to_f) * days_passed).round
+      else
+        return 0
+      end
+    end
+
+    def interval_days(interval)
+      case interval
+      when "monthly"
+        return COMMON_YEAR_DAYS_IN_MONTH[(parse_date(@date)).month]
+      when "weekly"
+        return 6
+      when "yearly"
+        return parse_date(@date).leap? ? 366 : 365
+      end
+    end
+
+    def pace(actual_visits, expected_visits)
+      # Pace = actual visits at point in time - expected visits to meet frequency at that point in time
+      actual_visits - expected_visits
+    end
+
+    def pace_indicator(pace)
+      if pace == 0
+        return "😁"
+      elsif pace > 0
+        return "🐇"
+      elsif pace < 0
+        return "🐢"
+      end
+    end
+
+    def parse_date(date)
+      Date.strptime(date, '%m-%d-%Y')
+    end
+
+    def parsed_non_business_days
+      @non_business_days.map do |day|
+        parse_date(day)
+      end
+    end
+
+    # days on which a session can hold
+    def business_days(start_date, end_date)
+      # remove saturdays and sundays
+      # remove holidays(array from Ambiki)
+      # remove school holidays/non business days
+      # should we remove today?(datetime call?)
+      working_days = get_working_days(start_date, end_date)
+
+      holidays = get_holidays(start_date, end_date)
+      
+      working_days.sort - parsed_non_business_days.sort - holidays.sort
+    end
+
+    # scoped to the interval
+    def remaining_visits(completed_visits:, required_visits:)
+      visits = required_visits.to_i - completed_visits.to_i
+      visits = 0 if visits < 0
+      visits
+    end
+
+    # scoped to the interval
+    def reset_date(start_date:, interval:)
+      case interval
+      when "monthly"
+        return reset_date_monthly(start_date, interval)
+      when "weekly"
+        return reset_date_weekly(start_date, interval)
+      when "yearly"
+        return reset_date_yearly(start_date)
+      end
+    end
+
+    # scoped to the interval
+    def start_of_treatment_date(start_date, interval="monthly")
+      case interval
+      when "monthly"
+        return start_of_treatment_date_monthly(start_date)
+      when "weekly"
+        return start_of_treatment_date_weekly(start_date)
+      when "yearly"
+        return start_of_treatment_date_yearly(start_date)
+      end
+    end
+
+    def get_working_days(start_date, end_date)
+      (start_date..end_date).filter do |day|
+        !(day.saturday? || day.sunday?)
+      end
+    end
+
+    def get_holidays(start_date, end_date)
+      Holidays.between(start_date, end_date, @state).map { |holiday| holiday[:date] }
+    end
+
+    # get actual date of the first day of the week where date falls
+    def week_start(date, offset_from_sunday=0)
+      date - ((date.wday - offset_from_sunday)%7)
+    end
+    
+    # reset date for the yearly interval
+    def reset_date_yearly(start_date)
+      (parse_date(@date).leap? ? parse_date(start_date) + 366 : parse_date(start_date) + 365).strftime("%m-%d-%Y")
+    end
+
+    # reset date for the monthly interval
+    def reset_date_monthly(start_date, interval)
+      (start_of_treatment_date(parse_date(start_date), interval) + COMMON_YEAR_DAYS_IN_MONTH[(parse_date(@date)).month]-1).strftime("%m-%d-%Y")
+    end
+
+    # reset date for the weekly interval
+    def reset_date_weekly(start_date, interval)
+      (start_of_treatment_date(parse_date(start_date), interval) + 7).strftime("%m-%d-%Y")
+    end
+
+    # start of treatment for the yearly interval
+    def start_of_treatment_date_yearly(start_date)
+      parse_date("#{start_date.month}-#{start_date.day}-#{parse_date(@date).year}")
+    end
+
+    # start of treatment for the montly interval
+    def start_of_treatment_date_monthly(start_date)
+      parse_date("#{parse_date(@date).month}-#{start_date.day}-#{parse_date(@date).year}")
+    end
+
+    # start of treatment for the weekly interval
+    def start_of_treatment_date_weekly(start_date)
+      date = parse_date(@date)
+
+      week_start_date = week_start(date)
+      weekly_date = week_start_date + start_date.wday
+      weekly_date = date < weekly_date ? weekly_date - 7 : weekly_date
+
+      weekly_date
+    end
   end
 end
--- pacing.gemspec
+++ pacing.gemspec
@@ -3,7 +3,7 @@ Gem::Specification.new do |s|
     s.version     = '0.1.0' # https://stackoverflow.com/questions/3728626/what-to-use-as-an-initial-version
     s.summary     = "Pacing is a tool that enables therapists to better manage and track their caseload."
     s.description = "Pacing is built for cases where there are therapy frequency limitations that need to be adhered to. For example, in the case of an [IEP (Individualized Education Program)](https://ambiki.com/glossary-concepts/iep), 504 plan, or a Services plan. This gem helps to calculate remaining visits as well as a therapist's current pace to meet visit mandates."
-    s.authors     = ["Kevin S. Dias", "Samuel Okoth"]
+    s.authors     = ["Kevin S. Dias", "Samuel Okoth", "Lukong I. Nsoseka"]
     s.email       = 'info@ambiki.com'
     s.files       = ["lib/pacing.rb"]
     s.homepage    =
